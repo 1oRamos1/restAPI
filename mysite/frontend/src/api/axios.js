cacfe8a23a518781526262774f912af3528201b1
Or Amos <amosor@mta.ac.il>
2025-09-03 20:47:37 +0300

    Prepare for Render deployment

--- mysite/frontend/src/api/axios.js
+++ mysite/frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/v1/tracker/',
+    baseURL: 'api/v1/tracker/',
     withCredentials: true,
     xsrfCookieName: 'csrftoken',
     xsrfHeaderName: 'X-CSRFToken',
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -93,9 +93,10 @@ MIDDLEWARE = [
 ]
 
 # === CORS & CSRF ===
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://tracker-2528.onrender.com",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
