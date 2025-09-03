e07b4f11e7e05f15fdf03a34ac77774d85fda08a
Or Amos <amosor@mta.ac.il>
2025-09-03 17:32:15 +0300

    Prepare for Railway deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from decouple import config
-
+import dj_database_url
 
 # === Security ===
 SECRET_KEY = config('SECRET_KEY')
@@ -83,6 +83,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -99,6 +100,7 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://tracker-production-387a.up.railway.app",
     "http://localhost:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
@@ -137,10 +139,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 # === Database ===
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 # === Password validators ===
