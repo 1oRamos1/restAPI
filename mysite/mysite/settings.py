1c0c88590b250dae4e83753e424ccc37f53069af
Or Amos <amosor@mta.ac.il>
2025-09-04 16:08:55 +0300

    Prepare for Render deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 
 # === Security ===
@@ -139,12 +140,21 @@ TEMPLATES = [
 WSGI_APPLICATION = "mysite.wsgi.application"
 
 # === Database ===
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if config("DATABASE_URL", default=None):
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=config("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # === Password validators ===
 AUTH_PASSWORD_VALIDATORS = [
