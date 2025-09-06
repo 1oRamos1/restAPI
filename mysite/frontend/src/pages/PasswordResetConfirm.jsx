f79b2ea3a8379ac79624239cf8e2c8cd1469f687
Or Amos <amosor@mta.ac.il>
2025-09-06 18:39:27 +0300

    Prepare for Railway deployment

--- mysite/frontend/src/pages/PasswordResetConfirm.jsx
+++ mysite/frontend/src/pages/PasswordResetConfirm.jsx
@@ -17,7 +17,7 @@ export default function PasswordResetConfirm() {
     }
 
     try {
-      await api.post('http://localhost:8000/dj-rest-auth/password/reset/confirm/', {
+      await api.post('https://tracker-2528.onrender.com/dj-rest-auth/password/reset/confirm/', {
         uid,
         token,
         new_password1: newPassword1,
