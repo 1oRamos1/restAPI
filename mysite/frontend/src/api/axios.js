b6234f6e2a2d9d87a4c166fb397a879d0fefd187
Or Amos <amosor@mta.ac.il>
2025-09-12 19:39:14 +0300

    Prepare for Render deployment

--- mysite/frontend/src/api/axios.js
+++ mysite/frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-    baseURL: 'https://tracker-2528.onrender.com/api/v1/tracker/',
+    baseURL: process.env.REACT_APP_API_URL
     withCredentials: true,
     xsrfCookieName: 'csrftoken',
     xsrfHeaderName: 'X-CSRFToken',
